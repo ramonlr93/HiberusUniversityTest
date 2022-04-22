714929710af66a2684a64ef475b4631b42bc9168
fl0rch <chamizo.flores@gmail.com>
2022-04-22 12:48:01 +0200

    test arreglado para dar true

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,8 +3,9 @@ import org.testng.annotations.Test;
 
 public class FirstTest {
   int num1 = 5;
-  int num2 = 3;
+  int num2 = 5;
 
+  //Inicio de los test
   @Test
   public void myFirstTest(){
     Assert.assertTrue(num1==num2,"Es el valor "+num1+" igual al valor "+num2+"");
