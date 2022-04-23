63b4e3974877b5443866b832731e51347518a25b
angellafuentesinues <lafuente.angel@gmail.com>
2022-04-23 23:48:17 +0200

    cambio num

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,8 +3,8 @@ import org.testng.annotations.Test;
 
 public class FirstTest {
   int num1 = 5;
-  int num2 = 3;
-
+  int num2 = 5;
+  //he cambiado el num2, el 3 por un 5
   @Test
   public void myFirstTest(){
     Assert.assertTrue(num1==num2,"Es el valor "+num1+" igual al valor "+num2+"");
