f2cf5282cc095b1af04fa959a3f9bccc6ef71526
mnavarro <marianavarropesos@gmail.com>
2022-05-19 08:24:08 +0200

    practicasgit2

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,8 +2,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-  int num1 = 5;
-  int num2 = 5;
+  int num1 = 3;
+  int num2 = 3;
 
   @Test
   public void myFirstTest(){
